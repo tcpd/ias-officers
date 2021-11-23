cfc74f4418cef28b645cface8bcfff76668f0609
maxDeCoder <vedjum@gmail.com>
2021-11-23 11:26:30 +0530

    add bubble plot

--- analysis/viz.py
+++ analysis/viz.py
@@ -3,17 +3,26 @@ import pandas as pd
 import numpy as np
 import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
-admin_categories = [
-    "Land Revenue Mgmt & District Admn",
-    "Personnel and General Administration",
-    "Finance"
-]
+colors = {}
+
+lower = 100
+
+def color_map(item):
+    return colors[item]
 
 @st.cache
 def load_unigram_data():
     df = pd.read_csv('./processed/unigram maps.csv')
     df = df.loc[df["Experience"] != "N.A."]
+
+    df["text"] = df["Count"].apply(lambda x: "size: "+str(x))
+
+    for i, item in enumerate(df["Subject"].unique()):
+        colors[item] = lower + i
+    
+    df["colors"] = df["Subject"].map(color_map)
     return df
 
 @st.cache
@@ -26,7 +35,7 @@ admin_categories = [
     "Finance"
 ]
 
-def filter_by_value(df, col, value, include_admin=True, number_of_rows=5, percentage=False):
+def filter_by_value(df, col, value, include_admin=True, number_of_rows=5, percentage=False, include_other=False):
 
     df = df.loc[df[col].str.contains(value)]
     df.sort_values(["Count"], ascending=False, inplace=True)
@@ -35,28 +44,77 @@ def filter_by_value(df, col, value, include_admin=True, number_of_rows=5, percen
         for category in admin_categories:
             df = df.loc[df["Experience"] != category]
 
-    df.drop(columns=[col], inplace=True)
-    df.set_index(df.columns[0], inplace=True)
+    temp = df[number_of_rows:]
+    df = pd.DataFrame(df.head(number_of_rows))
+    value_sum = temp["Count"].sum()
+    
+    if include_other:
+        df = df.append({
+            df.columns[0]: "Other",
+            df.columns[1]: "Other",
+            "Count": value_sum
+        }, ignore_index=True)
 
-    df = df.head(number_of_rows)
+    df.drop(columns=[col], inplace=True)
+    df.set_index(df.columns[0], inplace=True) 
 
     if percentage:
         sum = df["Count"].sum()
         df = pd.DataFrame(df["Count"].apply(lambda x: (x / sum) * 100))
 
-    return df
+    try:
+        return df.drop(columns=["text", "colors"])
+    except:
+        return df
+
+def scatter_plot(df, include_top_cat=True, min_value=50):
+    if include_top_cat:
+        df = df.loc[df["Experience"] != 'Land Revenue Mgmt & District Admn']
+        df = df.loc[df["Experience"] != 'Personnel and General Administration']
+        df = df.loc[df["Experience"] != 'Finance']
+
+    df = df.loc[df["Count"] > min_value]
+    max_value = df["Count"].max()
+
+    length_y_axis = df["Experience"].unique().shape[0]
+
+    fig = go.Figure(data=[go.Scatter(
+        x=df["Experience"].to_list(),
+        y=df["Subject"].to_list(),
+        text=df["text"].to_list(),
+        mode="markers",
+        marker=dict(
+            size=df["Count"].apply(lambda x: x*50/max_value).to_list(),
+            color = df["colors"].to_list(),
+        )
+    )])
+
+    fig = fig.update_layout(
+        autosize=False,
+        height=100 + length_y_axis * 25,
+        width=800,
+    )
+
+    # remove grid lines from the figure
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    
+    return fig
 
 unigram_data = load_unigram_data()
 lists_data = load_lists_data()
 
 st.subheader('Unigram maps')
-st.write(unigram_data)
+st.write(unigram_data.drop(columns=["text", "colors"]))
 
 
 st.subheader('Select a category of expirence')
 option_experience = st.selectbox("", unigram_data['Experience'].unique())
 
-filtered_df_experience = filter_by_value(unigram_data.copy(), 'Experience', option_experience, number_of_rows=500)
+include_other_experience = st.checkbox("Include combined remaining entries")
+number_of_rows_experience = st.slider('Number of rows', min_value=1, max_value=8, value=5)
+
+filtered_df_experience = filter_by_value(unigram_data.copy(), 'Experience', option_experience, number_of_rows=number_of_rows_experience, include_other=include_other_experience)
 st.write(filtered_df_experience)
 st.bar_chart(data=filtered_df_experience, height=600)
 
@@ -93,3 +151,8 @@ fig2.patch.set_facecolor('none')
 [ax2.texts[i].set_color('w') for i in range(len(ax2.texts))]
 ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 st.pyplot(fig2)
+
+include_admin_scatter = st.checkbox('Remove Top Admin Categorie')
+num_range = st.slider('Number of rows', min_value=1, max_value=600, value=20)
+
+st.plotly_chart(scatter_plot(unigram_data.copy(), include_top_cat=include_admin_scatter, min_value=num_range))
